d1a9996c3c0afa1cbd5bd8e6c1d66a8e9edbbc22
dstrauss <fmdenver@gmail.com>
2019-01-26 12:42:47 -0500

    wow.

--- src/BlogLib/BlogWebsite/Data/ApplicationDbContext.cs
+++ src/BlogLib/BlogWebsite/Data/ApplicationDbContext.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using BlogWebsite.Models;
+using BlogWebsite.Data;
 
 namespace BlogWebsite.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+
+        public class ApplicationDbContext : IdentityDbContext
         {
+            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options)
+            {
+            }
+
+            //protected override void OnModelCreating(ModelBuilder builder)
+            //{
+            //    base.OnModelCreating(builder);
+            //    // Customize the ASP.NET Core Identity model and override the defaults if needed.
+            //    // For example, you can rename the ASP.NET Core Identity table names and more.
+            //    // Add your customizations after calling base.OnModelCreating(builder);
+            //}
+
+            public DbSet<BlogWebsite.Models.Contact> Contact { get; set; }
         }
     }
-}
